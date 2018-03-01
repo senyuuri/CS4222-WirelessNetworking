6ee1cd14819577644aa1dd59b41fad0e3c499941
senyuuri <senyuuri@gmail.com>
2018-03-01 23:53:26 +0800

    Add zero crossing method

--- hw2/convert.py
+++ hw2/convert.py
@@ -3,13 +3,26 @@ Convert stdout readings to csv files
 
 For internal use. NOT to be submitted.
 """
-flist = ['1min30sec163steps', '1min30sec164hold', '1min30sec167steps', '1min30sec177hold']
+# flist = ['1min30sec163steps', '1min30sec164hold', '1min30sec167steps', '1min30sec177hold']
+
+# for f in flist:
+# 	with open('out/'+f, 'r') as fin:
+# 		with open('csv2/'+f+'.csv', 'w') as fout:
+# 			lines = fin.readlines()
+# 			for line in lines:
+# 				if 'G' in line:
+# 					parts = line.replace('G','').replace(' ','').split(',')
+# 					new = [str(round(float(x)*9.80, 2)) for x in parts]
+# 					fout.write(','.join(new)+'\n')
+
+flist = ['accel-interval-noiseless.file', 'accel-interval-noisy.file', 'accel-noiseless', 'accel-noisy', 'accel-vnoisy']
 
 for f in flist:
-	with open('out/'+f, 'r') as fin:
-		with open('csv/'+f+'.csv', 'w') as fout:
+	with open('archive/'+f, 'r') as fin:
+		with open('archive_csv/'+f+'.csv', 'w') as fout:
 			lines = fin.readlines()
 			for line in lines:
-				if 'G' in line:
-					fout.write(line.replace('G','').replace(' ',''))
+				parts = line.split(',')[2:]
+				new = [str(round(float(x)/9.80, 2)) for x in parts]
+				fout.write(','.join(new)+'\n')
 
