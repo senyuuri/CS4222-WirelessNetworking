42a730d206867342c83696083b59b680406c2b61
senyuuri <seiinatsuyuu@gmail.com>
2018-03-22 19:32:43 +0800

    Add write/read test

--- hw3/example-runicast.c
+++ hw3/example-runicast.c
@@ -37,10 +37,11 @@
  *         Adam Dunkels <adam@sics.se>
  */
 
-#include <stdio.h>
-
+#include <stdlib.h> 
 #include "contiki.h"
 #include "net/rime/rime.h"
+#include "board-peripherals.h"
+#include "node-id.h"
 
 #include "lib/list.h"
 #include "lib/memb.h"
@@ -51,9 +52,18 @@
 #define MAX_RETRANSMISSIONS 4
 #define NUM_HISTORY_ENTRIES 4
 
-/* read 1024 bytes at a time */
-#define CHUNK 1024 
+/* Number of readings to be generated/sent. Size of 10000 samples is about 32KB*/
+#define NUM_SAMPLE 10000
+/* send 1024 bytes at a time */
+#define PACKET_SIZE 1024 
+#define EXT_FLASH_BASE_ADDR_SENSOR_DATA     0 
+#define EXT_FLASH_MEMORY_END_ADDRESS        0x400010 
+#define EXT_FLASH_BASE_ADDR                 0
+#define EXT_FLASH_SIZE                      4*1024*1024
 
+/*---------------------------------------------------------------------------*/
+static int randint=-1;
+static int address_index_writing = -sizeof(randint);
 /*---------------------------------------------------------------------------*/
 PROCESS(test_runicast_process, "runicast test");
 AUTOSTART_PROCESSES(&test_runicast_process);
@@ -137,23 +147,63 @@ PROCESS_THREAD(test_runicast_process, ev, data)
     PROCESS_WAIT_EVENT_UNTIL(0);
   }
 
-  // Read a chunk of data from file
-  char buf[CHUNK];
-  FILE *file;
-  size_t nread;
+  /* generate 32KB random data and save to external storage */
+  srand(1);
+  leds_on(LEDS_YELLOW);
 
-  file = fopen("sample.csv", "r");
-  if (file) {
-      while ((nread = fread(buf, 1, sizeof buf, file)) > 0)
-          fwrite(buf, 1, nread, stdout);
-      if (ferror(file)) {
-          /* deal with error */
+  if(EXT_FLASH_BASE_ADDR_SENSOR_DATA + address_index_writing + sizeof(randint) < EXT_FLASH_MEMORY_END_ADDRESS){
+    int executed = ext_flash_open();
+    if(!executed) {
+      printf("Cannot open flash\n");
+      ext_flash_close();
+      return 0;
+    }
+    for(int i=0; i< NUM_SAMPLE; i++){
+      randint = rand() % 100;
+      if(i % 1000 == 0){
+        printf("WRITE PROGRESS:%d/10000\n", i); 
+        printf("WRITING TO EXTERNAL FLASH @0x%08X sizeof: %d\n",EXT_FLASH_BASE_ADDR_SENSOR_DATA + address_index_writing, sizeof(randint));
       }
-      fclose(file);
-  } else{
-      // file not found
-      printf("sample.csv not found.");
+      executed = ext_flash_write(EXT_FLASH_BASE_ADDR_SENSOR_DATA + address_index_writing, sizeof(randint), (uint8_t *)&randint);
+      if(!executed) {
+        printf("Error saving data to EXT flash\n");
+      }
+      address_index_writing += sizeof(randint);
+    }
+
+    
+    ext_flash_close();
+  }else{
+    printf("POSSIBLE EXTERNAL FLASH OVERFLOW @0x%08X sizeof: %d\n",EXT_FLASH_BASE_ADDR_SENSOR_DATA + address_index_writing, sizeof(randint));  
   }
+  leds_off(LEDS_YELLOW);
+
+
+  /* Read test */
+  static int address_offset = 0; 
+  static char sensor_data_int[1];
+
+  int executed = ext_flash_open();
+
+  if(!executed) {
+    printf("Cannot open flash\n");
+    ext_flash_close();
+    return 0;
+  }
+
+  int count = 0;
+  int pointer = EXT_FLASH_BASE_ADDR + address_offset;
+  while(pointer < sizeof(sensor_data_int) * NUM_SAMPLE){
+    executed = ext_flash_read(address_offset, sizeof(sensor_data_int),  (int *)&sensor_data_int);
+    count++;
+    printf("%08X,%d\n", pointer,sensor_data_int[0]);
+    address_offset += sizeof(sensor_data_int);
+    pointer = EXT_FLASH_BASE_ADDR + address_offset;
+  
+  }
+  ext_flash_close();
+  printf("%d samples read successfully", count);
+
 
   while(1) {
     static struct etimer et;
@@ -164,11 +214,9 @@ PROCESS_THREAD(test_runicast_process, ev, data)
     if(!runicast_is_transmitting(&runicast)) {
       linkaddr_t recv;
 
-      packetbuf_copyfrom("abcdefghijklmnopqrstuvwxyzAabcdefghijklmnopqrstuvwxyzAabcdefghijklmnopqrstuvwxyz\n", 54);
+      packetbuf_copyfrom("abcdefghijklmnopqrstuvwxyzAabcdefghijklmnopqrstuvwxyzA\n", 54);
       recv.u8[0] = 169;
       recv.u8[1] = 134;
-
-      printf("packet buffer: %s\n", (char *)  packetbuf_dataptr )
 
       printf("%u.%u: sending runicast to address %u.%u\n",
 	     linkaddr_node_addr.u8[0],
