d9c6f5cfef8c18e34b7cceb8501832ba33afa30d
senyuuri <seiinatsuyuu@gmail.com>
2018-04-20 15:20:05 +0800

    Complete BNB

--- hw4/bnb_trainer.py
+++ hw4/bnb_trainer.py
@@ -2,13 +2,24 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.externals import joblib
 import numpy
 
-data = numpy.loadtxt(open("training.csv", "rb"), delimiter=",")
+# training
+# ----------------------
+data = numpy.loadtxt(open("training.csv", "rb"), delimiter=",", skiprows=1)
 bnb = BernoulliNB()
-bnb.fit(data[:, 0], data[:, 1])
+bnb.fit(data[:, 1:8], data[:, 8])
+# dump trained model
+# joblib.dump(bnb, 'bnb_model.pkl') 
 
 
+# prediction
+# -----------------------
 # load Bernoulli Naive Bayes model
 # bnb = joblib.load('bnb_model.pkl') 
 
-# dump trained model
-# joblib.dump(bnb, 'bnb_model.pkl') 
+t_data = numpy.loadtxt(open("testing.csv", "rb"), delimiter=",")
+prediction = bnb.predict(t_data[:, 1:8])
+
+with open('output.csv', 'w') as fout:
+    for i in range(t_data.shape[0]):
+        line = str(t_data[i][0]) + ',' + str(prediction[i]) + '\n'
+        fout.write(line)
